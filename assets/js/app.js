481e8f0adbafaba9425398fc343066e1418542c8
flor condori <condorihuamanif@gmail.com>
2017-04-24 13:15:52 -0500

    mostrar img mediante url

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,22 @@
-app
+function creandoDiv(getUrl){
+	var contenedor = document.getElementById("contenedor");
+	var div = document.createElement("div");
+	var img = document.createElement("img");
+
+	div.classList.add("contenedor-img");
+	img.setAttribute("src",getUrl);
+
+	div.appendChild(img);
+	contenedor.appendChild(div);
+}
+
+function getUrl(e){
+	console.log(e);
+	if(e.keyCode == 13){
+		creandoDiv(this.value);
+	}
+	this.value = "";
+}
+
+var inputUrl = document.getElementById("url");
+inputUrl.onkeydown = getUrl;
