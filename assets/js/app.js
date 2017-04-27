a9d71e7b3dadb12d24a2fdee4db12c48036e29d7
flor condori <condorihuamanif@gmail.com>
2017-04-27 18:21:56 -0500

    insertar una flecha

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,31 @@
 function creandoDiv(getUrl){
 	var contenedor = document.getElementById("contenedor");
 	var div = document.createElement("div");
-	var img = document.createElement("img");
-
 	div.classList.add("contenedor-img");
+	var img = document.createElement("img");
 	img.setAttribute("src",getUrl);
 
+	var divFecha = document.createElement("div");
+	divFecha.setAttribute("id","div-abrir-venta");
+	divFecha.classList.add("div-abrir-venta");
+	var imgFecha = document.createElement("img");
+	imgFecha.setAttribute("src","assets/img/Expand Arrow-26.png");
+	divFecha.appendChild(imgFecha);
+
+	var divVentana = document.createElement("div");
+	divVentana.setAttribute("id","ventana-medidas");
+	divVentana.classList.add("ventana-medidas");
+	var inputWidth = document.createElement("input");
+	var inputHeight = document.createElement("input");
+	divVentana.appendChild(inputWidth);
+
+	div.appendChild(divVentana);
+	div.appendChild(divFecha);
 	div.appendChild(img);
 	contenedor.appendChild(div);
 }
 
 function getUrl(e){
-	console.log(e);
 	if(e.keyCode == 13){
 		creandoDiv(this.value);
 	}
@@ -20,3 +34,13 @@ function getUrl(e){
 
 var inputUrl = document.getElementById("url");
 inputUrl.onkeydown = getUrl;
+
+var flecha = document.getElementById("div-abrir-venta");
+console.log(flecha);
+flecha.onlick =function(){
+	document.getElementById("ventana-medidas").style.display = "inline-block";
+};
+
+
+
+
